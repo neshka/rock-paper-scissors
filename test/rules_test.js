24655840bb9f59902aeb3f9869e34b41ffca15cd
Peter Kofler <peter.kofler@code-cop.org>
2018-08-15 23:13:37 +0200

    Format.

--- test/rules_test.js
+++ test/rules_test.js
@@ -1,8 +1,10 @@
-var expect    = require("chai").expect;
+var expect = require("chai").expect;
 var rules = require("../app/rules");
 
 describe("rules of the game", function() {
+  
   describe("there is a winner", function() {
+    
     it("paper vs rock", function() {
         var winner = rules.compare('paper', 'rock');
         expect(winner).to.equal('paper')
