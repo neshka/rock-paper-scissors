6a378438d82813694e8331cffda91e87fb8b3a69
neshka <neshka_mind@yahoo.com>
2018-08-15 21:58:15 +0100

    refactor: exctract constans

--- app/rules.js
+++ app/rules.js
@@ -1,30 +1,31 @@
+var paper = 'paper';
+var rock = 'rock';
+var scissors = 'scissors';
 
-
-function compare (hand1, hand2) {
-    if (hand2 === 'paper') {
+function compare(hand1, hand2) {
+    if (hand2 === paper) {
         //make sure paper is always in hand1
         hand2 = hand1;
-        hand1 = 'paper';
+        hand1 = paper;
     }
 
-    if (hand1 === 'paper' && hand2 === 'rock') {
-        return 'paper';
+    if (hand1 === paper && hand2 === rock) {
+        return paper;
     }
 
-    if (hand1 === 'paper' && hand2 === 'scissors') {
-        return 'scissors';
+    if (hand1 === paper && hand2 === scissors) {
+        return scissors;
     }
 
-    if (hand2 === 'rock') {
+    if (hand2 === rock) {
         //make sure rock is always in hand1
         hand2 = hand1;
-        hand1 = 'rock';
+        hand1 = rock;
     }
 
-    if (hand1 === 'rock' && hand2 === 'scissors') {
-        return 'rock';
+    if (hand1 === rock && hand2 === scissors) {
+        return rock;
     }
 }
-
 
 module.exports.compare = compare;
