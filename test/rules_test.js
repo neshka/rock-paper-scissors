118f6d83197c040d90aded3eb4bd25a3b38a8599
neshka <neshka_mind@yahoo.com>
2018-08-15 20:53:44 +0100

    set up the testing environment

---
+++ test/rules_test.js
@@ -0,0 +1,11 @@
+var expect    = require("chai").expect;
+var converter = require("../app/rules");
+
+describe("rules of the game", function() {
+  describe("there is a winner", function() {
+    it("paper vs rock", function() {
+        
+    });
+  });
+
+});
