84aa7f34677498b7c14d63a164f6e2bbb18b0620
neshka <neshka_mind@yahoo.com>
2018-10-18 07:10:53 +0200

    add different code for rules

--- app/rules.js
+++ app/rules.js
@@ -9,71 +9,71 @@ function compare(hand1, hand2) {
     if (hand1 === hand2) {
         return draw;
     }
-    
-    if (hand2 === paper) {
-        //make sure paper is always in hand1
-        hand2 = hand1;
-        hand1 = paper;
+
+    if (hand1 === paper) {
+        switch (hand2) {
+            case scissors:
+                return scissors;
+                break;
+            case lizard:
+                return lizard;
+                break;
+            default:
+                return paper;
+        } 
     }
 
-    if (hand1 === paper && hand2 === rock) {
-        return paper;
-    }
+    if (hand1 === rock) {
+        switch (hand2) {
+            case paper:
+                return paper;
+                break;
+            case spock:
+                return spock;
+                break;
+            default:
+                return rock;
+        }
+    } 
 
-    if (hand1 === paper && hand2 === scissors) {
-        return scissors;
-    }
+    if (hand1 === scissors) {
+        switch (hand2) {
+            case rock:
+                return rock;
+                break;
+            case spock:
+                return spock;
+                break;
+            default:
+                return scissors;
+        }
+    } 
 
-    if (hand1 === paper && hand2 === lizard) {
-        return lizard;
-    }
+    if (hand1 === lizard) {
+        switch (hand2) {
+            case rock:
+                return rock;
+                break;
+            case scissors:
+                return scissors;
+                break;
+            default:
+                return lizard;
+        }
+    } 
 
-    if (hand1 === paper && hand2 === spock) {
-        return paper;
+    if (hand1 === spock) {
+        switch (hand2) {
+            case paper:
+                return paper;
+                break;
+            case lizard:
+                return lizard;
+                break;
+            default:
+                return spock;
+        }
     }
-
-    if (hand2 === rock) {
-        //make sure rock is always in hand1
-        hand2 = hand1;
-        hand1 = rock;
-    }
-
-    if (hand1 === rock && hand2 === scissors) {
-        return rock;
-    }
-
-    if (hand1 === rock && hand2 === lizard) {
-        return rock;
-    }
-
-    if (hand1 === rock && hand2 === spock) {
-        return spock;
-    }
-
-    if (hand2 === scissors) {
-        //make sure scissors are always in hand1
-        hand2 = hand1;
-        hand1 = scissors;
-    }
-
-    if (hand1 === scissors && hand2 === lizard) {
-        return scissors;
-    }
-
-    if (hand1 === scissors && hand2 === spock) {
-        return spock;
-    }
-
-    if (hand2 === spock) {
-        //make sure spock is always in hand1
-        hand2 = hand1;
-        hand1 = spock;
-    }
-
-    if (hand1 === spock && hand2 === lizard) {
-        return lizard;
-    }
-
 }
 
 module.exports.compare = compare;
---
+++ test/rules_test.1.js
@@ -0,0 +1,143 @@
+var expect = require("chai").expect;
+var rules = require("../app/rules");
+
+describe("rules of the game", function() {
+  
+  describe("there is a winner", function() {
+    
+    it("paper vs rock", function() {
+        var winner = rules.compare('paper', 'rock');
+        expect(winner).to.equal('paper')
+    });
+
+    //zmniejsze ilosc testow wtedy
+
+    it("paper vs spock", function() {
+        var winner = rules.compare('paper', 'spock');
+            expect(winner).to.equal('paper')
+    });
+
+    it("rock vs paper", function() {
+        var winner = rules.compare('rock', 'paper');
+        expect(winner).to.equal('paper')
+    });
+
+    it("spock vs paper", function() {
+        var winner = rules.compare('spock', 'paper');
+        expect(winner).to.equal('paper')
+    });
+    
+    it("paper vs scissors", function() {
+        var winner = rules.compare('paper', 'scissors');
+        expect(winner).to.equal('scissors')
+    });
+
+    it("paper vs lizard", function() {
+        var winner = rules.compare('paper', 'lizard');
+        expect(winner).to.equal('lizard')
+    });
+
+
+
+    it("paper vs paper", function() {
+        var winner = rules.compare('paper', 'paper');
+            expect(winner).to.equal('draw')
+    });
+
+
+
+    it("rock vs scissors", function() {
+        var winner = rules.compare('rock', 'scissors');
+        expect(winner).to.equal('rock')
+    });
+
+    it("rock vs lizard", function() {
+        var winner = rules.compare('rock', 'lizard');
+        expect(winner).to.equal('rock')
+    });
+
+    it("rock vs spock", function() {
+        var winner = rules.compare('rock', 'spock');
+        expect(winner).to.equal('spock')
+    });
+
+    it("rock vs rock", function() {
+        var winner = rules.compare('rock', 'rock');
+        expect(winner).to.equal('draw')
+    });
+
+    it("scissors vs rock", function() {
+        var winner = rules.compare('scissors', 'rock');
+        expect(winner).to.equal('rock')
+    });
+
+    it("scissors vs paper", function() {
+        var winner = rules.compare('scissors', 'paper');
+        expect(winner).to.equal('scissors')
+    });
+
+    it("scissors vs lizard", function() {
+        var winner = rules.compare('scissors', 'lizard');
+        expect(winner).to.equal('scissors')
+    });
+
+    it("scissors vs spock", function() {
+        var winner = rules.compare('scissors', 'spock');
+        expect(winner).to.equal('spock')
+    });
+
+    it("scissors vs scissors", function() {
+        var winner = rules.compare('scissors', 'scissors');
+        expect(winner).to.equal('draw')
+    });
+
+    it("spock vs scissors", function() {
+        var winner = rules.compare('spock', 'scissors');
+        expect(winner).to.equal('spock')
+    });
+
+    it("spock vs rock", function() {
+        var winner = rules.compare('spock', 'rock');
+        expect(winner).to.equal('spock')
+    });
+
+
+
+    it("spock vs lizard", function() {
+        var winner = rules.compare('spock', 'lizard');
+        expect(winner).to.equal('lizard')
+    });
+
+    it("spock vs spock", function() {
+        var winner = rules.compare('spock', 'spock');
+        expect(winner).to.equal('draw')
+    });
+
+    it("lizard vs scissors", function() {
+        var winner = rules.compare('lizard', 'scissors');
+        expect(winner).to.equal('scissors')
+    });
+
+    it("lizard vs rock", function() {
+        var winner = rules.compare('lizard', 'rock');
+        expect(winner).to.equal('rock')
+    });
+
+    it("lizard vs paper", function() {
+        var winner = rules.compare('lizard', 'paper');
+        expect(winner).to.equal('lizard')
+    });
+
+    it("lizard vs spock", function() {
+        var winner = rules.compare('lizard', 'spock');
+        expect(winner).to.equal('lizard')
+    });
+
+    it("lizard vs lizard", function() {
+        var winner = rules.compare('lizard', 'lizard');
+        expect(winner).to.equal('draw')
+    });
+
+  });
+
+});
